a15bbc3ae8313b7e6792072d2b03f86e6067a9d8
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-01 20:27:15 -0400

    created post ability for the admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+# super user = caleb, password
+class Post(models.Model):
+    """Blog Post"""
+
+    title = models.CharField(max_length=200)
+
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="posts")
+
+    body = models.TextField()
+
+    create = models.DateTimeField(auto_now_add=True)
+
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Get absolute url for this instance"""
+
+        return reverse("post_details", kwargs={"pk": self.pk})
