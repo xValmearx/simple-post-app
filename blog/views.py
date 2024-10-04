f56c3a93ec598c345ca19c6f873e73a5e22bb4f6
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-03 20:34:07 -0400

    created page for detailed post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -8,3 +8,10 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
+
+class BlogDetialView(DetailView):
+    """Blog Post Detailed View"""
+
+    model = Post
+    template_name = "post_detail.html"
