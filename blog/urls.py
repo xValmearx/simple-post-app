f8eb67067fbc7acb1b40463240f9ae2163326787
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-08 20:25:36 -0400

    created a post page to add new blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetialView
+from .views import BlogListView, BlogDetialView, BlogCreateView
 
 urlpatterns = [
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", BlogDetialView.as_view(), name="post_detail"),
     path("", BlogListView.as_view(), name="home"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
 
+from django.views.generic.edit import CreateView
+
 # Create your views here.
 
 from .models import Post
@@ -15,3 +17,8 @@ class BlogDetialView(DetailView):
 
     model = Post
     template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title","author","body"]
